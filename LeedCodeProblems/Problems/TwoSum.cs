6daf35626fc3e111e4553220826017b394c0abbf
Gubarenko-Evgenij <gubarenko.evgenii@yandex.ru>
2023-07-02 12:18:26 +0300

    initial commit

---
+++ LeedCodeProblems/Problems/TwoSum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCodeProblems.Problems
+{
+    public static class TwoSum
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            return nums;
+        }
+    }
+}
---
+++ LeedCodeProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeedCodeProblems.Problems;
+
+var nums = new int[4] { 2, 7, 11, 15 };
+var target = 9;
+
+var res = TwoSum.TwoSum(nums, target);
+
+foreach (var item in res)
+{
+    Console.WriteLine(item);
+}
+Console.ReadLine();
