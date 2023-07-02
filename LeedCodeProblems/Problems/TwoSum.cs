cfef0ba8816bcf111f2f53de0ef0a2959636cf4e
Gubarenko-Evgenij <gubarenko.evgenii@yandex.ru>
2023-07-02 14:19:52 +0300

    Adding new solve task

--- LeedCodeProblems/Problems/TwoSum.cs
+++ LeedCodeProblems/Problems/TwoSum.cs
@@ -8,10 +8,57 @@ namespace LeedCodeProblems.Problems
 {
     public static class TwoSum
     {
-        public static int[] TwoSum(int[] nums, int target)
+        public static int[] Two_sum(int[] nums, int target)
         {
 
-            return nums;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                int twoNumber = target - nums[i];
+                int indexTwoNumber = -1;
+                indexTwoNumber = Array.LastIndexOf(nums, twoNumber);
+                if (indexTwoNumber > i)
+                {
+                    return new int[] { i, indexTwoNumber };
+                }
+            }
+            return null;
+        }
+
+        public static void TestResult(int[] res, int[] result)
+        {
+            for (var i = 0; i < 2; ++i)
+            {
+                if (result[i] != res[i])
+                {
+                    throw new Exception("Invalid result!");
+                }
+            }
+        }
+        public static void TestTwoSum()
+        {
+            var nums = new int[4] { 2, 7, 11, 15 };
+            var target = 9;
+            var result = new int[] { 0, 1 };
+            var res = TwoSum.Two_sum(nums, target);
+            TestResult(res, result);
+           
+            nums = new int[] { 3, 2, 4 };
+            target = 6;
+            result = new int[] { 1, 2 };
+            res = TwoSum.Two_sum(nums, target);
+            TestResult(res, result);
+
+            nums = new int[] { 3, 3 };
+            target = 6;
+            result = new int[] { 0, 1 };
+            res = TwoSum.Two_sum(nums, target);
+            TestResult(res, result);
+
+            nums = new int[] { -1, -2, -3, -4, -5 };
+            target = -8;
+            result = new int[] { 2, 4 };
+            res = TwoSum.Two_sum(nums, target);
+            TestResult(res, result);
         }
     }
 }
--- LeedCodeProblems/Program.cs
+++ LeedCodeProblems/Program.cs
@@ -1,12 +1,4 @@
 ﻿using LeedCodeProblems.Problems;
 
-var nums = new int[4] { 2, 7, 11, 15 };
-var target = 9;
-
-var res = TwoSum.TwoSum(nums, target);
-
-foreach (var item in res)
-{
-    Console.WriteLine(item);
-}
+TwoSum.TestTwoSum();
 Console.ReadLine();
