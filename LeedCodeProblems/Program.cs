dfff358f169bf03a08b7c266ba8259aedce13913
Gubarenko-Evgenij <gubarenko.evgenii@yandex.ru>
2023-07-06 21:10:16 +0300

    add solution Valid Parentheses

--- LeedCodeProblems/Program.cs
+++ LeedCodeProblems/Program.cs
@@ -1,8 +1,16 @@
 ﻿using LeedCodeProblems.Problems;
+using System;
 
 TwoSum.TestTwoSum();
+Console.WriteLine("Two Sum - Ok");
 Palindrome.TestIsPalindrome();
+Console.WriteLine("Palindrome - Ok");
 PalindromeAlt.TestIsPalindrome();
+Console.WriteLine("Palindrome Alt - Ok");
 RomanToInteger.TestRomanToInteger();
+Console.WriteLine("Roman To Integer - Ok");
 LongestCommonPrefix.TestLongestCommonPrefix();
+Console.WriteLine("Longest Common Prefix - Ok");
+ValidParentheses.TestValidParentheses();
+Console.WriteLine("Valid Parentheses - Ok");
 Console.ReadLine();
